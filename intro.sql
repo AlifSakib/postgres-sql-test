3cc7225ded69d98de91ba0a2b75015a7218dd1bf
alifsakib <alifsakib@gmail.com>
2023-08-05 21:18:14 +0600

    insert multiple value, delete rows

--- intro.sql
+++ intro.sql
@@ -10,16 +10,37 @@
 
 -- Remane a TABLE
 
--- CREATE TABLE users3 (
---     user_id SERIAL PRIMARY KEY,
---     username VARCHAR(255) UNIQUE NOT NULL,
---     email VARCHAR(255) UNIQUE NOT NULL,
---     age INT DEFAULT 18
---     -- PRIMARY KEY(user_id, username)
---     -- UNIQUE( username, email)
--- )
+CREATE TABLE users3 (
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    age INT DEFAULT 18
+    -- PRIMARY KEY(user_id, username)
+    -- UNIQUE( username, email)
+)
 
 -- ## Insert data into a TABLE
--- INSERT INTO users3 values (1, 'john', 'jsohn@gmail.com')
+INSERT INTO users3 values (1, 'john', 'jsohn@gmail.com')
 
-select * from users3
+-- select * from users3
+
+-- 1. insert INTO table_name (column1, column2, column3, ...) values (value1, value2, value3, ...);
+-- 2. insert INTO table_name values (value1, value2, value3, ...);
+
+-- RETURNING * column_name
+
+-- insert multiple values
+-- insert into table_name values (value1, value2, value3), (value1, value2, value3), (value1, value2, value3);
+
+INsert INTO users3 (user_id, username, email,age) values (2,'alif', 'alif@gmail.com', 35);
+
+INSERT INTO users3 (username, email) values 
+('alif1', 'alif1@as.com'),
+('alif2', 'alif2@as.com'),
+('alif3', 'alif3@as.com');
+
+--Truncate Delete all row in a table WITHOUT the table , DROP delete the table -- interview question
+TRUNCATE TABLE users3;
+
+SELECT * from users3 
+
